590781d0e38ac64b0a7ce477300fcf5ce490449f
knightian <ian@whiteknightit.com.au>
2022-08-27 05:13:52 +1000

    fixes

--- FFPP/Versions/api/Bootstrap/BootstrapRoutes.cs
+++ FFPP/Versions/api/Bootstrap/BootstrapRoutes.cs
@@ -40,7 +40,7 @@ namespace FFPP.Api.Bootstrap
                 {
                     Task<GraphTokenUrl> getUrl = new(() =>
                     {
-                        return new() { url = $"https://login.microsoftonline.com/{ApiEnvironment.Secrets.TenantId}/oauth2/v2.0/authorize?scope=https://graph.microsoft.com/.default+offline_access+openid+profile&response_type=code&client_id={ApiEnvironment.Secrets.ApplicationId}&redirect_uri={ApiEnvironment.FfppFrontEndUri}/bootstrap/ReceiveGraphToken" };
+                        return new() { url = $"https://login.microsoftonline.com/{ApiEnvironment.Secrets.TenantId}/oauth2/v2.0/authorize?scope=https://graph.microsoft.com/.default+offline_access+openid+profile&response_type=code&client_id={ApiEnvironment.Secrets.ApplicationId}&redirect_uri={ApiEnvironment.FfppFrontEndUri}/bootstrap/receivegraphtoken" };
                     });
 
                     getUrl.Start();
@@ -61,56 +61,50 @@ namespace FFPP.Api.Bootstrap
             {
                 if (!ApiEnvironment.IsBoostrapped)
                 {
-                    Task tokenTask = new(async () =>
+                    HttpRequestMessage requestMessage = new(HttpMethod.Post, $"https://login.microsoftonline.com/{ApiEnvironment.Secrets.TenantId}/oauth2/v2.0/token");
+                    requestMessage.Content = new StringContent($"client_id={ApiEnvironment.Secrets.ApplicationId}&scope=https://graph.microsoft.com/.default+offline_access+openid+profile&code={code}&redirect_uri={ApiEnvironment.FfppFrontEndUri}/bootstrap/receivegraphtoken&grant_type=authorization_code&client_secret={ApiEnvironment.Secrets.ApplicationSecret}");
+
+                    requestMessage.Content.Headers.ContentType = new MediaTypeHeaderValue("application/x-www-form-urlencoded");
+
+                    HttpResponseMessage responseMessage = await RequestHelper.SendHttpRequest(requestMessage);
+
+                    if (responseMessage.IsSuccessStatusCode)
                     {
-                        HttpRequestMessage requestMessage = new(HttpMethod.Post, $"https://login.microsoftonline.com/{ApiEnvironment.Secrets.TenantId}/oauth2/v2.0/token");
-                        requestMessage.Content = new StringContent($"client_id={ApiEnvironment.Secrets.ApplicationId}&scope=https://graph.microsoft.com/.default+offline_access+openid+profile&code={code}&redirect_uri={ApiEnvironment.FfppFrontEndUri}/bootstrap/ReceiveGraphToken&grant_type=authorization_code&client_secret={ApiEnvironment.Secrets.ApplicationSecret}");
+                        string responseRawString = await responseMessage.Content.ReadAsStringAsync();
+                        string[] headersRaw = responseRawString[1..^1].Split(",");
 
-                        requestMessage.Content.Headers.ContentType = new MediaTypeHeaderValue("application/x-www-form-urlencoded");
+                        Dictionary<string, string> headers = new();
 
-                        HttpResponseMessage responseMessage = await RequestHelper.SendHttpRequest(requestMessage);
-
-                        if (responseMessage.IsSuccessStatusCode)
+                        foreach (string headerPairRaw in headersRaw)
                         {
-                            string responseRawString = await responseMessage.Content.ReadAsStringAsync();
-                            string[] headersRaw = responseRawString[1..^1].Split(",");
+                            string[] h = headerPairRaw.Split(":");
 
-                            Dictionary<string, string> headers = new();
-
-                            foreach (string headerPairRaw in headersRaw)
+                            if (h[0].StartsWith('"'))
                             {
-                                string[] h = headerPairRaw.Split(":");
-
-                                if (h[0].StartsWith('"'))
-                                {
-                                    // Trim the redundant " char from start and end of string
-                                    h[0] = h[0][1..^1];
-                                }
-
-                                if (h[1].StartsWith('"'))
-                                {
-                                    h[1] = h[1][1..^1];
-                                }
-
-                                headers.Add(h[0], h[1]);
+                                // Trim the redundant " char from start and end of string
+                                h[0] = h[0][1..^1];
                             }
 
-                            ApiEnvironment.Secrets.RefreshToken = headers["refresh_token"];
-                            ApiZeroConfiguration zeroConf = await ApiZeroConfiguration.Read();
-                            zeroConf.RefreshToken = ApiEnvironment.Secrets.RefreshToken;
-                            zeroConf.Save();
-                            //redirect to success page
-                            //context.Response.Redirect("https://localhost:7074/index.html");
-                        }
-                        else
-                        {
-                            //redirect to error page
-                            //context.Response.Redirect("https://localhost:7074/index.html");
-                        }
-                    });
+                            if (h[1].StartsWith('"'))
+                            {
+                                h[1] = h[1][1..^1];
+                            }
 
-                    tokenTask.Start();
-                    await tokenTask;
+                            headers.Add(h[0], h[1]);
+                        }
+
+                        ApiEnvironment.Secrets.RefreshToken = headers["refresh_token"];
+                        ApiZeroConfiguration zeroConf = await ApiZeroConfiguration.Read();
+                        zeroConf.RefreshToken = ApiEnvironment.Secrets.RefreshToken;
+                        zeroConf.Save();
+                        //redirect to success page
+                        context.Response.Redirect($"{ApiEnvironment.FfppFrontEndUri}/setup/graphtoken/success");
+                    }
+                    else
+                    {
+                        //redirect to error page
+                        context.Response.Redirect($"{ApiEnvironment.FfppFrontEndUri}/setup/graphtoken/error");
+                    }
                 }
                 else
                 {
